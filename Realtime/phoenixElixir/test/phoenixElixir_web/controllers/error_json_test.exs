a836463be02435f5076715ce411bbda432288ad2
Bhashith Udawaththa <bhashith.me@gmail.com>
2025-01-07 23:49:53 +0530

    Phoenix Realtime: Initiated the project

---
+++ Realtime/phoenixElixir/test/phoenixElixir_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule PhoenixElixirWeb.ErrorJSONTest do
+  use PhoenixElixirWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert PhoenixElixirWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert PhoenixElixirWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end

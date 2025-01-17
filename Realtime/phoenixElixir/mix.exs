f3818f69a8ede78b88c0debd9f655b9648633440
Bhashith Udawaththa <bhashith.me@gmail.com>
2025-01-17 13:14:58 +0530

    Phoenix realtime : Updated release info

--- Realtime/phoenixElixir/mix.exs
+++ Realtime/phoenixElixir/mix.exs
@@ -3,7 +3,7 @@ defmodule PhoenixElixir.MixProject do
 
   def project do
     [
-      app: :phoenixElixir,
+      app: :phoenix_elixir,
       version: "0.1.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),

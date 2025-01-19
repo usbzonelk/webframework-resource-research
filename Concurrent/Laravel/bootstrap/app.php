872ff3fb0800ce05dbf32b2369f3eba881131540
Bhashith Udawaththa <73486769+usbzonelk@users.noreply.github.com>
2025-01-19 12:31:44 +0000

    Concurrent Laravel : Finalized project

---
+++ Concurrent/Laravel/bootstrap/app.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/up',
+        using: function () {
+            Route::middleware('api')
+                ->group(base_path('routes/api.php'));
+        },
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
